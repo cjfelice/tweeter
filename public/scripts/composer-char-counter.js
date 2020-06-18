f2613fd3ae896d71892cf2c0e57dc62966e7aeb3
cjfelice <monocularmedia@gmail.com>
2020-06-18 06:58:10 +0000

    added tweet css layouts

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-  console.log('yomomma');
   $('#inputBox').on('input', function() {
     let val = $(this).val();
     $(this).parent().children().find('.counter').html(140 - val.length);
     if (140 - val.length < 0) {
       $(this).parent().children().find('.counter').css('color', 'red');
+    }
+    if (140 - val.length >= 0) {
+      $(this).parent().children().find('.counter').css('color', '#545149');
     }
   });
 });
