5b6837b35ac53f1f1ae29fb0f51e49ae0817b45f
cjfelice <monocularmedia@gmail.com>
2020-06-18 07:28:52 +0000

    added hover functionality for old tweets in js

---
+++ public/scripts/tweetHover.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.tweet').mouseover(function() {
+    $(this).css('box-shadow', '4px 4px grey');
+    $(this).find('.tag').css('color', 'grey');
+  });
+  $('.tweet').mouseout(function(){
+    $(this).css('box-shadow', '0px 0px');
+    $(this).find('.tag').css('color', 'rgb(235, 230, 230)');
+  });
+});
