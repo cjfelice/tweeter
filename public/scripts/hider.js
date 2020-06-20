2a6dadd7a77ee56acb2d0f3002a69422182f2e85
cjfelice <monocularmedia@gmail.com>
2020-06-20 04:43:15 +0000

    added back to top button with animation to the bottom of page

--- public/scripts/hider.js
+++ public/scripts/hider.js
@@ -1,7 +1,9 @@
 //hides the error message and tweet entry bar on page-load//
 // and handles animation for entry bar and button press//
+// also handles floating 'back-to-top' logic//
 $(document).ready(function() {
   $('.error').hide();
+  $('.arrow').hide();
   $('.new-tweet').hide();
   $('#new-tweet-button').mouseover(function() {
     $('#bouncer').css('animation-duration', '0.2s');
@@ -13,5 +15,17 @@ $(document).ready(function() {
     $('.new-tweet').slideDown('slow');
     $('#bouncer').css('animation-name', 'none');
     $('#bouncer').slideUp();
+  });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 350) {
+      $('.arrow').slideDown('slow');
+    }
+    if ($(this).scrollTop() < 350) {
+      $('.arrow').slideUp('slow');
+    }
+    $('.arrow').on('click', function() {
+      $([document.documentElement, document.body]).scrollTop('.container');
+      $('.new-tweet').slideDown('slow');
+    });
   });
 });
