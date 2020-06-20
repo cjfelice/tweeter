c6faf51d4616860e5ca41f55ce3737953cdbcd34
cjfelice <monocularmedia@gmail.com>
2020-06-20 00:35:34 +0000

    fixed css styling and scaling for mobile

--- public/scripts/hider.js
+++ public/scripts/hider.js
@@ -1,3 +1,17 @@
+//hides the error message and tweet entry bar on page-load//
+// and handles animation for entry bar and button press//
 $(document).ready(function() {
   $('.error').hide();
+  $('.new-tweet').hide();
+  $('#new-tweet-button').mouseover(function() {
+    $('#bouncer').css('animation-duration', '0.2s');
+  });
+  $('#new-tweet-button').mouseout(function() {
+    $('#bouncer').css('animation-duration', '1s');
+  });
+  $('#new-tweet-button').click(function() {
+    $('.new-tweet').slideDown('slow');
+    $('#bouncer').css('animation-name', 'none');
+    $('#bouncer').slideUp();
+  });
 });
